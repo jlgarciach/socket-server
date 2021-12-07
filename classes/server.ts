077444df537f5b56cd5b734e1912c1dfa2e3ac2e
Jorge García <jlgarcia-@hotmail.com>
2021-12-06 18:32:25 -0600

    Sección 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,51 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
-
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/socket';
 
 export default class Server {
+    private static _instance: Server;
+
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server( this.app );
+        // this.io = new socketIO.Server( this.httpServer );
+        // this.io = socketIO( this.httpServer, { cors: { origin: true, credential: true}});
+        // this.io = socketIO( this.httpServer, { origin: '*:*'});
+        this.io = new socketIO.Server( this.httpServer, { cors: { origin: true, credentials: true } } )
+
+        this.escucharSockets();
+
     }
 
-    start( callback: any ) {
-        this.app.listen( this.port, callback );
+    public static get instance() {
+        return this._instance || ( this._instance = new this() );
+    }
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado...');
+
+            // Mensaje
+            socket.mensaje( cliente, this.io );
+
+            // Desconectar
+            socket.desconectar( cliente );
+        });
+    }
+
+    start( callback: (() => void) ) {
+        this.httpServer.listen( this.port, callback );
     }
 
 }
